2833485d4dbe87ecc2515533e83a9318ab18bbc0
Daniyal Amjad <dadaniyal.amjad@yahoo.com>
2020-07-08 18:55:15 +0500

    side bar

--- src/screens/Dashboard/DashboardScreen.jsx
+++ src/screens/Dashboard/DashboardScreen.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Layout, Breadcrumb } from 'antd';
 
 import './DashboardScreen.css'
 import HeaderS from '../../components/Header/Header'
+import SidebarS from '../../components/Sidebar/Sidebar'
 
 
-const { Content, Sider } = Layout;
-const { SubMenu } = Menu;
+const { Content } = Layout;
 
 export default class DashboardScreen extends Component {
   render() {
@@ -15,26 +14,11 @@ export default class DashboardScreen extends Component {
       <Layout style={{ height: '100vh', overflowY: "hidden" }}>
         <HeaderS />
         <Layout>
-          <Sider trigger={null}
-            collapsible
-            breakpoint="lg"
-            collapsedWidth="78"
-            onCollapse={(collapsed, type) => { }}>
-            <Menu
-              onClick={this.handleClick}
-              defaultSelectedKeys={['dashboard']}
-            >
-              <Menu.Item key="dashboard">Dashboard</Menu.Item>
-              <Menu.Item key="category">Category</Menu.Item>
-              <Menu.Item key="items">Items</Menu.Item>
-            </Menu>
-          </Sider>
+          <SidebarS />
           <Layout>
             <Content style={{ padding: '0 50px' }}>
               <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
+                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
               </Breadcrumb>
               <div className="site-layout-content">
                 Content
@@ -64,123 +48,7 @@ export default class DashboardScreen extends Component {
                 Content
                 <br />
                 Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
-                Content
-                <br />
+
               </div>
             </Content>
           </Layout>
